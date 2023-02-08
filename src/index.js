3203d1c667fb4a1d96495c8832951cba5f763e52
Anton Berisha <43340489+islo123@users.noreply.github.com>
2023-02-08 17:04:16 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <HashRouter>
-        <Routes>
+        <Routes history={hashHistory}>
           <Route exact path="*" element={<EmployeeContextProvider><AuthContextProvider><App /></AuthContextProvider></EmployeeContextProvider>}>
           </Route>    
         </Routes>
